0505ed2f24dfd860e685b8b297da6b12483466a6
마도 <mydodo1120@naver.com>
2025-12-19 21:01:51 +0900

    ytdlp 사용

--- function/player.js
+++ function/player.js
@@ -1,62 +1,128 @@
-const ytdl = require("ytdl-core");
-const { createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
+const ffmpegPath = require("ffmpeg-static");
+const path = require("path");
+
+const ytdlpPath = path.join(
+  __dirname,
+  "..",
+  "local_modules",
+  "yt-dlp.exe"
+);
+
+const { spawn } = require('child_process');
+const { createAudioResource, StreamType, AudioPlayerStatus } = require("@discordjs/voice");
 const { GetVideoDetails } = require('youtube-search-api');
-const { addQueue, firstQueueDelete } = require("../scripts/queue");
+const { QueueAdd, QueueShift } = require("../scripts/queue");
 
 
-exports.playing = function(url, connection, interaction, client, queue) {
+exports.StartPlayer = function(url, connection, interaction, client, queue) 
+{
     let analyzeQueue = queue.get(interaction.guild.id)
-    player_play(analyzeQueue, interaction, url)
+    MusicPlaying(analyzeQueue, interaction, url);
 
-    analyzeQueue.player.on('error', (error) => {
-        console.error(error);
-        interaction.channel.send("**❗재생 오류가 발생했어요.**");
-        if(analyzeQueue.name.length == 1) queue.delete(interaction.guild.id) 
-        else player.stop()
-    })
+    analyzeQueue.player.on(AudioPlayerStatus.Idle, async () => 
+    {
+        analyzeQueue.Stop(false);
 
-    analyzeQueue.player.on(AudioPlayerStatus.Idle, async () => {
         if(!analyzeQueue) return;
-
-        if(analyzeQueue.repeat == "on") addQueue(analyzeQueue, analyzeQueue.url[0], analyzeQueue.name[0], analyzeQueue.thumbnail[0])
-        
-        if(analyzeQueue.name.length > 1) {
-
-            firstQueueDelete(analyzeQueue)
-            return player_play(analyzeQueue, interaction, analyzeQueue.url[0])
-
+        if(analyzeQueue.repeat == "on") 
+        {
+            QueueAdd(analyzeQueue, analyzeQueue.url[0], analyzeQueue.name[0], analyzeQueue.thumbnail[0])
         } 
 
-        if(analyzeQueue.Mixing == "on") {
-            try {
+        if(analyzeQueue.name.length > 1 || analyzeQueue.repeat == "on") 
+        {
+            QueueShift(analyzeQueue)
+            MusicPlaying(analyzeQueue, interaction, analyzeQueue.url[0])
+            return;
+        }
 
-                firstQueueDelete(analyzeQueue);                    
-                await Mix(analyzeQueue);
-                return player_play(analyzeQueue, interaction, analyzeQueue.url[0])
+        if(analyzeQueue.Mixing == "on") 
+        {
+            try 
+            {
+                await QueueMixing(analyzeQueue);
+                QueueShift(analyzeQueue);       
+                MusicPlaying(analyzeQueue, interaction, analyzeQueue.url[0]);
 
-            } catch(error) {
-
+                return;
+            } 
+            catch(error)
+            {
                 interaction.channel.send("**⏹ __믹스 생성중 오류가 발생했어요. 나중에 다시 시도해주세요.__**")
                 analyzeQueue.Mixing = "off";
-                if(analyzeQueue.length <= 1) connection.destroy();
-                return queue.delete(interaction.guild.id)
+
+                if(analyzeQueue.length <= 1) 
+                {
+                    queue.delete(interaction.guild.id);
+                    connection.destroy();
+
+                    return;
+                }
+                
+                MusicPlaying(analyzeQueue, interaction, analyzeQueue.url[0]);
+                return;
             }
         } 
-        interaction.channel.send("**⏹️ __재생이 완료되었어요!__**")
-        connection.destroy();
-        queue.delete(interaction.guild.id)
+
+        if(analyzeQueue.name.length == 1 && analyzeQueue.repeat == "off" && analyzeQueue.Mixing == "off")
+        {
+            interaction.channel.send("**⏹️ __재생이 완료되었어요!__**")
+
+            analyzeQueue.Stop(true);
+        }
+    })
+
+    analyzeQueue.player.on('error', (error) => 
+    {
+        console.error(error);
+        interaction.channel.send("**❗재생 오류가 발생했어요.**");
+
+        if(analyzeQueue.name.length <= 1)
+        {
+            queue.delete(interaction.guild.id) 
+            return;
+        }
+
+        analyzeQueue.player.stop();
+        return;
     })
 }
 
-function player_play(queue, interaction, url) {
-    const resource = createAudioResource(ytdl(url, {filter: 'audioonly', quality: 'highestaudio', highWaterMark: 1 << 25 }), {highWaterMark: 1})
-    queue.player.play(resource)
+function MusicPlaying(queue, interaction, url) 
+{
+  const ytdlp = spawn(ytdlpPath, [
+    "-f", "bestaudio",
+    "--js-runtime", "node",
+    "-o", "-",
+    url
+  ], { stdio: ["ignore", "pipe", "pipe"] });
+
+  const ffmpeg = spawn(ffmpegPath, [
+    "-loglevel", "0",
+    "-i", "pipe:0",
+    "-map_metadata", "-1",
+    "-f", "opus",
+    "-ac", "2",
+    "-ar", "48000",
+    "-b:a", "128k",
+    "pipe:1"
+  ], { stdio: ["pipe", "pipe", "ignore"] });
+
+  ytdlp.stdout.pipe(ffmpeg.stdin);
+  queue.modules = { ytdlp: ytdlp, ffmpeg: ffmpeg };   
+
+  const resource = createAudioResource(ffmpeg.stdout, {
+    inputType: StreamType.OggOpus
+  });
+
+  queue.player.play(resource);
 }
 
 
-async function Mix(queue) {
+
+async function QueueMixing(queue) {
     const userData = await GetVideoDetails(queue.id[0])
     Object.values(userData.suggestion).forEach(database => {
-        addQueue(queue, "https://www.youtube.com/watch?v="+ database.id, database.title, `https://img.youtube.com/vi/${database.id}/maxresdefault.jpg`)
+        QueueAdd(queue, "https://www.youtube.com/watch?v="+ database.id, database.title, `https://img.youtube.com/vi/${database.id}/maxresdefault.jpg`)
     });
 }
